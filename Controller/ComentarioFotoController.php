d309c00e1b3a15d5083f6192a9d5e82965f085f4
Chrysweel <pablitoen05@hotmail.com>
2013-03-11 09:10:44 +0100

    error redeclare commentcontroller

--- Controller/ComentarioFotoController.php
+++ Controller/ComentarioFotoController.php
@@ -9,7 +9,7 @@ use sdfs\sdfsBundle\Entity\Comment;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class CommentFotoController extends Controller
+class ComentarioFotoController extends Controller
 {	
 	public function ultimoComentarioAction(){
 		$em = $this->get('doctrine')->getEntityManager();
